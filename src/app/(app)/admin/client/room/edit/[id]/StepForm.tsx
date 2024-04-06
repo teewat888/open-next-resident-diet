a77b2da60fef97c6c68731e4c556360d381d999c
Teerawat P. <teewat@yahoo.com>
2024-04-07 08:00:31 +1000

    update to use react hook form shadcn

--- src/app/(app)/admin/client/room/edit/[id]/StepForm.tsx
+++ src/app/(app)/admin/client/room/edit/[id]/StepForm.tsx
@@ -23,23 +23,37 @@ import { Client, Wing } from '@/lib/db/schema';
 import { useEffect, useState } from 'react';
 import { ClientRoomForm } from './ClientRoomForm';
 import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 import FieldErrorClient from '@/components/composed/FieldErrorClient';
+import { useForm } from 'react-hook-form';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
 
 const locationSchema = z.object({
-  wing: z.string().min(1, 'Select a wing'),
-  room: z.string().min(1, 'Select a room'),
+  wing: z.string().min(1, 'Please select a wing'),
+  room: z.string().min(1, 'Please select a room'),
 });
 
 const StepForm = ({ clientId }: { clientId: string }) => {
   const [wings, setWings] = useState([] as Wing[]);
   const [client, setClient] = useState({} as Client);
   const [rooms, setRooms] = useState([] as AvailableRoom[]);
-  const [selectedRoom, setSelectedRoom] = useState('');
-  const [selectedWing, setSelectedWing] = useState('' as Wing['id'] | '0');
-  const [validationErrors, setValidationErrors] = useState(
-    null as null | z.ZodIssue[]
-  );
+
   const [isClientAlreadyInRoom, setIsClientAlreadyInRoom] = useState(false);
+
+  const form = useForm<z.infer<typeof locationSchema>>({
+    resolver: zodResolver(locationSchema),
+    defaultValues: {
+      wing: '',
+      room: '',
+    },
+  });
 
   const wingOptions = wings.map((wing) => (
     <SelectItem key={wing.id} value={wing.id.toString()}>
@@ -58,25 +72,12 @@ const StepForm = ({ clientId }: { clientId: string }) => {
     (async () => {
       const rooms = await getAvailableRooms(value);
       setRooms(rooms);
-      setSelectedWing(value as Wing['id'] | '0');
     })();
   };
 
-  const handleSubmission = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    // const formData = new FormData(e.currentTarget);
-    const result = locationSchema.safeParse({
-      wing: selectedWing.toString(),
-      room: selectedRoom,
-    });
-
-    if (result.success) {
-      setValidationErrors(null);
-    } else {
-      setValidationErrors(result.error.issues);
-    }
+  const onSubmit = (values: z.infer<typeof locationSchema>) => {
+    console.log(values);
   };
-
   useEffect(() => {
     (async () => {
       const isClientAlreadyInRoom = await isClientInRoom(clientId);
@@ -92,96 +93,118 @@ const StepForm = ({ clientId }: { clientId: string }) => {
       setClient(client[0]);
     })();
   }, []);
-  console.log('validationErrors', validationErrors);
+
   return (
     <>
       <div className='relative flex-col items-start gap-8 md:flex pt-6'>
-        <form
-          onSubmit={handleSubmission}
-          className='grid w-full items-start gap-6'
-        >
-          <div className='grid grid-cols-1 md:grid-cols-1 gap-6 md:w-1/2 m-auto'>
-            <fieldset
-              className={`grid gap-6 rounded-lg border p-4 
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className='grid w-full items-start gap-6'
+          >
+            <div className='grid grid-cols-1 md:grid-cols-1 gap-6 md:w-1/2 m-auto'>
+              <fieldset
+                className={`grid gap-6 rounded-lg border p-4 
               }`}
-            >
-              <legend className='-ml-1 px-1 text-sm font-medium'>
-                {client.firstName ? (
-                  `Location for ${client.firstName} ${client.lastName}`
-                ) : (
-                  <Skeleton className='h-4 w-[200px]' />
-                )}
-              </legend>
-              <div className='grid gap-3'>
-                <Label htmlFor='wing'>Select a wing</Label>
-                <Select name={'wing'} onValueChange={handleSelectWing}>
-                  <SelectTrigger>
-                    <SelectValue placeholder='Select a wing' />
-                  </SelectTrigger>
-                  <SelectContent>{wingOptions}</SelectContent>
-                </Select>
-                <FieldErrorClient
-                  fieldName='wing'
-                  errors={validationErrors ?? []}
-                />
-              </div>
-              {rooms.length > 0 && (
-                <>
-                  <div className='grid gap-3'>
-                    <Label htmlFor='room'>Select a room</Label>
-                    <Select
-                      name={'room'}
-                      onValueChange={(e: string) => {
-                        setSelectedRoom(e);
-                      }}
-                    >
-                      <SelectTrigger>
-                        <SelectValue placeholder='Select a room' />
-                      </SelectTrigger>
-                      <SelectContent>{roomOptions}</SelectContent>
-                    </Select>
-                  </div>
-                  <FieldErrorClient
-                    fieldName='room'
-                    errors={validationErrors ?? []}
-                  />
-                </>
-              )}
-              {selectedRoom && (
-                <>
-                  <div className='flex gap-4'>
-                    <ClientRoomForm
-                      mode={'permanent'}
-                      client={client}
-                      selectedRoom={selectedRoom}
-                      selectedWing={selectedWing.toString()}
-                    />
-                    <ClientRoomForm
-                      mode={'temporary'}
-                      client={client}
-                      selectedRoom={selectedRoom}
-                      selectedWing={selectedWing.toString()}
-                    />
-                  </div>
-                </>
-              )}
-            </fieldset>
-            <div className='flex justify-end gap-4'>
-              <Button type='button' className='w-1/2' variant={'secondary'}>
-                Skip
-              </Button>
-              <Button
-                type='submit'
-                className='w-1/2'
-                disabled={
-                  !isClientAlreadyInRoom && !!selectedRoom && !!selectedWing
-                }
               >
-                Next
-              </Button>
+                <legend className='-ml-1 px-1 text-sm font-medium'>
+                  {client.firstName ? (
+                    `Location for ${client.firstName} ${client.lastName}`
+                  ) : (
+                    <Skeleton className='h-4 w-[200px]' />
+                  )}
+                </legend>
+                <div className='grid gap-3'>
+                  <FormField
+                    control={form.control}
+                    name='wing'
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormControl>
+                          <Select
+                            value={field.value}
+                            name={field.name}
+                            onValueChange={(value) => {
+                              field.onChange(value);
+                              handleSelectWing(value);
+                            }}
+                          >
+                            <SelectTrigger>
+                              <SelectValue
+                                placeholder='Select a wing'
+                                ref={field.ref}
+                              />
+                            </SelectTrigger>
+                            <SelectContent>{wingOptions}</SelectContent>
+                          </Select>
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                {rooms.length > 0 && (
+                  <>
+                    <div className='grid gap-3'>
+                      <FormField
+                        control={form.control}
+                        name='room'
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormControl>
+                              <Select
+                                value={field.value}
+                                name={field.name}
+                                onValueChange={(value) => {
+                                  field.onChange(value);
+                                }}
+                              >
+                                <SelectTrigger>
+                                  <SelectValue
+                                    placeholder='Select a room'
+                                    ref={field.ref}
+                                  />
+                                </SelectTrigger>
+                                <SelectContent>{roomOptions}</SelectContent>
+                              </Select>
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                    </div>
+                  </>
+                )}
+                {form.getValues('room') && (
+                  <>
+                    <div className='flex gap-4'>
+                      <ClientRoomForm
+                        mode={'permanent'}
+                        client={client}
+                        selectedRoom={form.getValues('room')}
+                        selectedWing={form.getValues('wing').toString()}
+                      />
+                      <ClientRoomForm
+                        mode={'temporary'}
+                        client={client}
+                        selectedRoom={form.getValues('room')}
+                        selectedWing={form.getValues('wing').toString()}
+                      />
+                    </div>
+                  </>
+                )}
+              </fieldset>
+              <div className='flex justify-end gap-4'>
+                <Button type='button' className='w-1/2' variant={'secondary'}>
+                  Skip
+                </Button>
+                <Button type='submit' className='w-1/2'>
+                  Next
+                </Button>
+              </div>
             </div>
-          </div>
-        </form>
+          </form>
+        </Form>
       </div>
     </>
   );
